d0436dd43b320a9c3e19ce5557238a10d6797367
James Manning <james.manning@gmail.com>
2015-12-27 03:18:09 -0500

    leverage the structured data for the css and wrapping

--- src/scripts/emote.jsx
+++ src/scripts/emote.jsx
@@ -5,25 +5,30 @@ let _APNG = require("apng-canvas")
 let APNG = window.APNG
 
 let Bem = require("./berrymotes.jsx")
-let {EmoteParser} = require("emotes")
+let {EmoteParser, EmoteHtml} = require("emotes")
 let parser = new EmoteParser()
+let html = new EmoteHtml(Bem.map)
 const MAX_HEIGHT = 200
 
 module.exports = class Emote extends React.Component {
 	constructor(props) {
 		super(props)
-		var emoteIdentifier, originalString
+		var emoteIdentifier, originalString, htmlOutputData
+		// emoteId will be set when this is used via search results
 		if(props.emoteId) {
 			emoteIdentifier = props.emoteId
 			originalString = props.emoteId
+			htmlOutputData = html.getEmoteHtmlMetadataForEmoteName(props.emoteId)
 		} else {
 			let emoteObject = parser.parse(props.emote)
 			emoteIdentifier = emoteObject.emoteIdentifier
 			originalString = emoteObject.originalString
+			htmlOutputData = html.getEmoteHtmlMetadataForObject(emoteObject)
 		}
 		this.state = {
 			originalString,
 			emoteIdentifier,
+			htmlOutputData,
 			emoteData: Bem.findEmote(emoteIdentifier)
 		}
 		Bem.on("update", this.onEmoteUpdate.bind(this))
@@ -31,7 +36,8 @@ module.exports = class Emote extends React.Component {
 
 	onEmoteUpdate() {
 		this.setState({
-			emoteData: Bem.findEmote(this.state.emoteIdentifier)
+			emoteData: Bem.findEmote(this.state.emoteIdentifier),
+			htmlOutputData: html.getEmoteHtmlMetadataForEmoteName(this.state.emoteIdentifier)
 		})
 	}
 
@@ -63,55 +69,74 @@ module.exports = class Emote extends React.Component {
 
 	render() {
 		let emoteData = this.state.emoteData
-		if(!emoteData) {
+		let htmlOutputData = this.state.htmlOutputData
+
+		if(!htmlOutputData) {
 			return <span>{this.state.originalString}</span>
 		} else {
 
-			let title = `${(emoteData.names||[]).join(",")} from /r/${emoteData.sr}`
-			let className = {
-				"berrymotes": true,
-				"nsfw": emoteData.nsfw
+			// emotes have berryemote class set automatically, but we need berrymotes set as well
+			let className = htmlOutputData.cssClassesForEmoteNode
+			className['berrymotes'] = true
+
+			// workaround for the emotes package not currently just setting the properties directly
+			let emoteNodeStyle  = {}
+			htmlOutputData.cssStylesForEmoteNode.forEach((st) => {
+				emoteNodeStyle[st.propertyName] = st.propertyValue
+			})
+
+			let emoteNode = (
+				<span ref="emote"
+							onClick={this.props.emoteSelected && this.props.emoteSelected.bind(this, this.props.emoteIdentifier)}
+							className={cx(className)}
+							title={htmlOutputData.titleForEmoteNode}
+							style={emoteNodeStyle}/>
+			)
+
+			// provide a wrapper node if necessary to apply the styles/classes from the 'parent node' info
+			if (htmlOutputData.cssClassesForParentNode.length > 0 || htmlOutputData.cssStylesForParentNode.length > 0) {
+				// workaround for the emotes package not currently just setting the properties directly
+				let parentNodeStyle  = {}
+				htmlOutputData.cssStylesForParentNode.forEach((st) => {
+					parentNodeStyle[st.propertyName] = st.propertyValue
+				})
+
+				emoteNode = (
+					<span className={cx(htmlOutputData.cssClassesForParentNode)} style={parentNodeStyle}>
+						{emoteNode}
+					</span>
+				)
 			}
 
-			let scale = emoteData.height > MAX_HEIGHT ? MAX_HEIGHT/emoteData.height : 1
+			// provide wrapping to implement scaling down to meet MAX_HEIGHT requirement
+			if (emoteData.height > MAX_HEIGHT) {
+				let scale = MAX_HEIGHT/emoteData.height
 
-			let outerStyle = {
-				transform: `scale(${scale})`,
-				transformOrigin: "left top 0px",
-				position: "absolute",
-				top: 0,
-				left: 0
-			}
-
-			let style  = {
-				height: `${emoteData.height}px`,
-				width: `${emoteData.width}px`,
-				display: "inline-block",
-				position: "relative",
-				overflow: "hidden",
-				backgroundPosition: (emoteData["background-position"] || ["0px", "0px"]).join(" "),
-				backgroundImage: `url(${emoteData["background-image"]})`
-			}
-
-			let emoteNode = <span ref="emote" onClick={this.props.emoteSelected && this.props.emoteSelected.bind(this, this.props.emoteIdentifier)} className={cx(className)} title={title} style={style}/>
-			if(scale < 1) {
-				var outerWrapperStyle = {
+				let outerWrapperStyle = {
 					height: MAX_HEIGHT,
 					width: emoteData.width * scale,
 					position: "relative",
 					display: "inline-block"
 				}
 
-				return (
+				let innerWrapperStyle = {
+					transform: `scale(${scale})`,
+					transformOrigin: "left top 0px",
+					position: "absolute",
+					top: 0,
+					left: 0
+				}
+
+				emoteNode = (
 					<span className="berrymotes-wrapper-outer" style={outerWrapperStyle}>
-						<span className="berrymotes-wrapper" style={outerStyle}>
+						<span className="berrymotes-wrapper" style={innerWrapperStyle}>
 							{emoteNode}
 						</span>
 					</span>
 				)
-			} else {
-				return emoteNode
 			}
+
+			return emoteNode
 		}
 	}
 }
